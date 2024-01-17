3b90eaa81810a9fde78a04d94072f24730b36bfd
poojarose <156880882+poojarose@users.noreply.github.com>
2024-01-17 11:33:35 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+function calculate(){
+
+    debugger
+var totalamount= 0;
+var pooja=document.getElementById("pooja").value;
+var pooja1=document.getElementById("pooja1").value;
+var ankit=document.getElementById("ankit").value;
+
+
+}
